b47369b42fccf4bd1e871c1278a8bfd8ff469143
Pasquale Boemio <boemianrapsodi@gmail.com>
2019-11-11 01:19:35 +0100

    added example app

---
+++ examples/ios/app/app/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  app
+//
+//  Created by Pasquale Boemio on 11/11/2019.
+//  Copyright © 2019 Pasquale Boemio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ examples/ios/app/app/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  app
+//
+//  Created by Pasquale Boemio on 11/11/2019.
+//  Copyright © 2019 Pasquale Boemio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
